0bcb133c110591e09b5738cd10e546668becee72
Christie Maduna <ferggiee@gmail.com>
2021-12-14 13:55:21 +0200

    start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,20 @@
   <div class="home">
     <BlogPost :post="welcomeScreen" />
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
-    <div class="blog-card-wrap"></div>
+    <div class="blog-card-wrap">
     <div class="container">
       <h3>View More Recent Blogs</h3>
       <div class="blog-cards">
        <BlogCards :post="post" v-for="(post, index) in sampleBlogCards" :key="index"/>
+       </div>
+      </div>
+    </div>
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a post. Register for your free account today!</h2>
+        <router-link class="router-button" to="#">
+          Register for FireBlogs <Arrow class="arrow arrow-light"/>
+        </router-link>
       </div>
     </div>
   </div>
@@ -15,9 +24,10 @@
 <script>
 import BlogPost from '@/components/BlogPost';
 import BlogCards from '@/components/BlogCard';
+import Arrow from "@/assets/Icons/arrow-right-light.svg";
 export default {
     name: "Home",
-    components: { BlogPost, BlogCards },
+    components: { BlogPost, BlogCards, Arrow },
     data() {
       return {
         welcomeScreen: {
@@ -60,4 +70,38 @@ export default {
   }
 }
 
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media(min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+}
 </style>
